c844087c0fbd781322d17c64229bbf5a799e0418
Aslam <iupin5212@gmail.com>
2021-03-16 18:54:03 +0700

    update config

--- config/bri.php
+++ config/bri.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'api_v1' => '',
-    'api_v2' => '',
-
+    'api_url_v1' => env('BRI_API_URL_V1', 'https://sandbox.partner.api.bri.co.id'),
+    'api_url_v2' => env('BRI_API_URL_V2', 'https://partner.api.bri.co.id/sandbox'),
+    'client_id' => env('BRI_CLIENT_ID', 'AhkqwhpO8GAWGqY1vbZpa3vSWnj1KK9S'),
+    'client_secret' => env('BRI_CLIENT_SECRET', 'vGsfo8oMzbRPf2t4'),
 ];
